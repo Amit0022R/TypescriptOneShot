248f090ad6352b53dadabd846d1628a909966d3f
Amit Kumar <144479526+Amit0022R@users.noreply.github.com>
2025-05-01 15:34:13 +0530

    basics ts

---
+++ basics/dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+// console.log("Ts is working");
+// alert("Ts is working");
+// let a = 25;
+// a = "2"; 
+// this will run in js file but error in ts file
+// console.log(a);
+let a = "AMiT Kumar";
+let x = 25;
+let check = true;
---
+++ basics/src/index.ts
@@ -0,0 +1,23 @@
+// console.log("Ts is working");
+// alert("Ts is working");
+
+
+// let a = 25;
+// a = "2"; 
+// this will run in js file but error in ts file
+// console.log(a);
+
+let a : string = "AMiT Kumar";
+let x : number = 25;
+let check : boolean = true;
+
+
+
+
+
+
+
+
+
+
+
